0bd939a26853ecf951732840c5cc1d98d7a30db8
Avinash <avinash@Avinashs-MacBook-Pro-2.local>
2025-06-13 22:40:59 +0530

    litellm disable validatioon

--- litellm/llms/bedrock/chat/converse_handler.py
+++ litellm/llms/bedrock/chat/converse_handler.py
@@ -222,12 +222,43 @@ class BedrockConverseLLM(BaseAWSLLM):
             client = client  # type: ignore
 
         try:
-            response = await client.post(
-                url=api_base,
-                headers=headers,
-                data=data,
-                logging_obj=logging_obj,
-            )  # type: ignore
+            if headers.get('X_EMERGENT_LAZY_EXECUTION_RESPONSE') == 'true':
+                # call llm proxy to get response from db.
+                # Extract host from api_base
+                parsed_url = urlparse(api_base)
+                host = f"{parsed_url.scheme}://{parsed_url.netloc}"
+                response = client.get(
+                    url=f"{host}/response/lazy",
+                    params={'request_id': headers['request_id'], 'hash': headers['hash']},
+                    headers=headers,
+                )
+            else:
+                response = await client.post(
+                    url=api_base,
+                    headers=headers,
+                    data=data,
+                    logging_obj=logging_obj,
+                )  # type: ignore
+                response.raise_for_status()
+            if headers.get('X_EMERGENT_LAZY_EXECUTION') == 'true':
+                # handle llm proxy response after saving request payload in db
+                # creating dummy response to hanlde parsing. Actual response is hash and request_id from llm proxy
+                # dummy response field are just placeholders for parsing and will be rejected in downstream code.
+                # custom_response is actual response from llm proxy.
+                response_json = json.loads(response.text)
+                base_response = '{"candidates": [{"content": {"parts": [{"text": "Okay, lets start building the card game application."}], "role": "model"}, "finishReason": "STOP", "avgLogprobs": -0.11055656651745735}], "usageMetadata": {"promptTokenCount": 8213, "candidatesTokenCount": 663, "totalTokenCount": 8876, "promptTokensDetails": [{"modality": "TEXT", "tokenCount": 8213}], "candidatesTokensDetails": [{"modality": "TEXT", "tokenCount": 663}]}, "modelVersion": "gemini-2.0-flash", "responseId": "zDM8aNzKB7jFnvgPocrJaA"}'
+                base_response_json = json.loads(base_response)
+                base_response_json["custom_response"] = response_json
+                modified_json = json.dumps(base_response_json)
+                new_headers = dict(response.headers)
+                if 'content-encoding' in new_headers:
+                    del new_headers['content-encoding']
+                response = httpx.Response(
+                    status_code=response.status_code,
+                    headers=new_headers,
+                    content=modified_json.encode('utf-8'),
+                    request=response.request
+                )
             response.raise_for_status()
         except httpx.HTTPStatusError as err:
             error_code = err.response.status_code
